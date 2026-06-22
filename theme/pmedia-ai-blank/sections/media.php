7a67d1a9795ca620ba04f41f3ac1f339c73e0f37
trankhacvinh <trankhacvinh@users.noreply.github.com>
2026-06-23 06:43:59 +0700

    Add inline aspect ratio style for media embeds

--- theme/pmedia-ai-blank/sections/media.php
+++ theme/pmedia-ai-blank/sections/media.php
@@ -9,7 +9,9 @@ $poster = (string) pmedia_ai_section_value($pmedia_section, 'poster', '');
 $alt = (string) pmedia_ai_section_value($pmedia_section, 'alt', '');
 $caption = (string) pmedia_ai_section_value($pmedia_section, 'caption', '');
 $link = (string) pmedia_ai_section_value($pmedia_section, 'link', '');
-$ratio = sanitize_html_class(str_replace('/', '-', (string) pmedia_ai_section_value($pmedia_section, 'aspect_ratio', 'auto')));
+$ratio_raw = (string) pmedia_ai_section_value($pmedia_section, 'aspect_ratio', 'auto');
+$ratio = sanitize_html_class(str_replace('/', '-', $ratio_raw));
+$ratio_style = in_array($ratio_raw, ['1/1', '4/3', '16/9', '21/9'], true) ? 'aspect-ratio: ' . str_replace('/', ' / ', $ratio_raw) . ';' : '';
 $fit = sanitize_html_class((string) pmedia_ai_section_value($pmedia_section, 'object_fit', 'cover'));
 $controls = (bool) pmedia_ai_section_value($pmedia_section, 'controls', true);
 $autoplay = (bool) pmedia_ai_section_value($pmedia_section, 'autoplay', false);
@@ -18,7 +20,7 @@ $loop = (bool) pmedia_ai_section_value($pmedia_section, 'loop', false);
 ?>
 <section <?php echo pmedia_ai_section_attrs($pmedia_section, 'pmedia-section pmedia-media-section'); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>>
     <div class="pmedia-container">
-        <figure class="pmedia-media pmedia-ratio-<?php echo esc_attr($ratio); ?> pmedia-object-<?php echo esc_attr($fit); ?>">
+        <figure class="pmedia-media pmedia-ratio-<?php echo esc_attr($ratio); ?> pmedia-object-<?php echo esc_attr($fit); ?>" <?php if ($ratio_style !== '') : ?>style="<?php echo esc_attr($ratio_style); ?>"<?php endif; ?>>
             <?php if ($url !== '' && $type === 'video') : ?>
                 <video class="pmedia-media-el" <?php echo $controls ? 'controls' : ''; ?> <?php echo $autoplay ? 'autoplay playsinline' : ''; ?> <?php echo $muted ? 'muted' : ''; ?> <?php echo $loop ? 'loop' : ''; ?> <?php if ($poster !== '') : ?>poster="<?php echo esc_url($poster); ?>"<?php endif; ?>>
                     <source src="<?php echo esc_url($url); ?>">
